2a5be3c89cf98a206648dafebff5660dbc3daee3
Marco Crepaldi <marco.crepaldi@gmail.com>
2018-03-30 09:15:18 +0200

    filter queries by slug, structure_slug and publish_state

--- app/graphql/binda_api/fields/components_field.rb
+++ app/graphql/binda_api/fields/components_field.rb
@@ -1,5 +1,10 @@
 BindaApi::Fields::ComponentsField = GraphQL::Field.define do
   name 'components'
+
+  argument :slug, types.String
+  argument :structure_slug, types.String
+  argument :publish_state, types.String
+
   type BindaApi::Types::ComponentType.connection_type
 
   resolve(BindaApi::Resolvers::ComponentsResolver.new)
